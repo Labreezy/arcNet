721171874d29295f0f4694a3781745d934cd329e
aze <ajbtee@gmail.com>
2019-04-25 00:38:59 -0400

    Prototyping widgets

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,25 +1,25 @@
-import com.jogamp.opengl.util.texture.spi.DDSImage.D3DFMT_UNKNOWN
 import glm_.vec2.Vec2
 import glm_.vec4.Vec4
-import graphics.scenery.spirvcrossj.SPIRType.BaseType.Image
 import imgui.Cond
 import imgui.ImGui
-import imgui.WindowFlag
+import imgui.imgui.demo.showExampleApp.PlayerOverlay
 import imgui.or
+import imgui.WindowFlag as Wf
 
 val WINDOW_NAME = "arcNet  -  DISCONNECTED \uD83D\uDCE1 ..."
 val WINDOW_TINT = Vec4(0.16f, 0.16f, 0.16f, 1.0f)
 val WINDOW_HORZ = 640
 val WINDOW_VERT = 480
+var yup = true
 
 fun runApplication() {
     ImGui.run {
         setNextWindowPos(Vec2(0, 0), Cond.Always)
         setNextWindowSize(Vec2(WINDOW_HORZ, WINDOW_VERT), Cond.Always)
-        begin("", null, 0 or WindowFlag.NoTitleBar or WindowFlag.NoResize or WindowFlag.NoCollapse or WindowFlag.NoBackground)
+        begin("", null, 0 or Wf.NoTitleBar or Wf.NoResize or Wf.NoCollapse or Wf.NoBackground)
         text("Application average %.3f ms/frame (%.1f FPS)", 1_000f / io.framerate, io.framerate)
-
         end()
+        PlayerOverlay.invoke(::yup)
     }
 }
 
---
+++ src/main/kotlin/examples/PlayerOverlay.kt
@@ -0,0 +1,55 @@
+package imgui.imgui.demo.showExampleApp
+
+import gli_.has
+import glm_.vec2.Vec2
+import imgui.*
+import imgui.ImGui.font
+import imgui.ImGui.fontSize
+import imgui.ImGui.io
+import imgui.ImGui.isMousePosValid
+import imgui.ImGui.menuItem
+import imgui.ImGui.separator
+import imgui.ImGui.setNextWindowBgAlpha
+import imgui.ImGui.setNextWindowPos
+import imgui.ImGui.text
+import imgui.functionalProgramming.menuItem
+import imgui.functionalProgramming.popupContextWindow
+import imgui.functionalProgramming.withWindow
+import kotlin.reflect.KMutableProperty0
+import imgui.ColorEditFlag as Cef
+import imgui.InputTextFlag as Itf
+import imgui.SelectableFlag as Sf
+import imgui.TreeNodeFlag as Tnf
+import imgui.WindowFlag as Wf
+
+object PlayerOverlay {
+
+    var corner = 0
+
+    /** Demonstrate creating a simple static window with no decoration + a context-menu to choose which corner
+     *  of the screen to use */
+    operator fun invoke(open: KMutableProperty0<Boolean>) {
+
+        val DISTANCE = 32f
+        var flags = Wf.NoTitleBar or Wf.NoResize or Wf.AlwaysAutoResize or Wf.NoSavedSettings or Wf.NoFocusOnAppearing or Wf.NoNav
+        if (corner != -1) {
+            val windowPos = Vec2{ if (corner has it + 1) io.displaySize[it] - DISTANCE else DISTANCE }
+            val windowPosPivot = Vec2(if (corner has 1) 1f else 0f, if (corner has 2) 1f else 0f)
+            setNextWindowPos(windowPos, Cond.Always, windowPosPivot)
+        }
+        setNextWindowBgAlpha(0.0f)  // Transparent background
+        withWindow("title", open, flags) {
+            text("aze  -  [ ID 1234567890 ]")
+            separator()
+            ImGui.pushItemWidth(fontSize * -12)
+            text("Slayer")
+            text("Chain: 4  [ W:12 / M:32 ]")
+            ImGui.sameLine(240)
+            text("Bounty: 123,000,100 W$")
+            text("Mouse Position: " + when {
+                isMousePosValid() -> "(%.1f,%.1f)".format(io.mousePos.x, io.mousePos.y)
+                else -> "<invalid>"
+            })
+        }
+    }
+}
