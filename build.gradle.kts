15fc6d0095e2016eef2baf71b687269d253360dc
aze <ajbtee@gmail.com>
2019-04-24 20:24:39 -0400

    ImGui works

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.internal.os.OperatingSystem
 
 plugins {
     kotlin("jvm") version "1.3.30"
+    application
 }
 
 group = "com.azedevs"
@@ -9,13 +11,102 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+    maven("https://dl.bintray.com/kotlin/kotlin-dev")
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://jitpack.io")
 }
+
+val lwjglVersion = "3.2.1"
+val lwjglNatives = "natives-windows"
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("no.tornado:tornadofx:1.7.18")
+    implementation("com.github.kotlin-graphics:imgui:v1.68.01-00")
+
+    implementation("org.lwjgl", "lwjgl", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-assimp", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-bgfx", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-cuda", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-egl", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-glfw", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-jawt", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-jemalloc", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-libdivide", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-llvm", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-lmdb", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-lz4", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-meow", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-nanovg", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-nfd", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-nuklear", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-odbc", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-openal", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-opencl", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-opengl", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-opengles", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-openvr", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-opus", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-ovr", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-par", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-remotery", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-rpmalloc", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-sse", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-stb", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-tinyexr", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-tinyfd", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-tootle", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-vma", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-vulkan", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-xxhash", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-yoga", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-zstd", lwjglVersion)
+    runtimeOnly("org.lwjgl", "lwjgl", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-assimp", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-bgfx", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-jemalloc", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-libdivide", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-llvm", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-lmdb", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-lz4", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-meow", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nanovg", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nfd", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nuklear", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengles", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openvr", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opus", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-ovr", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-par", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-remotery", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-rpmalloc", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-sse", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-tinyexr", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-tinyfd", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-tootle", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-vma", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-xxhash", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-yoga", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-zstd", lwjglVersion, classifier = lwjglNatives)
+    
+//    val lwjglNatives = when (OperatingSystem.current()) {
+//        OperatingSystem.WINDOWS -> "natives-windows"
+//        OperatingSystem.LINUX   -> "natives-linux"
+//        OperatingSystem.MAC_OS  -> "natives-macos"
+//        else                    -> ""
+//    }
+//
+//    // Look up which modules and versions of LWJGL are required and add setup the approriate natives.
+//    configurations["compile"].resolvedConfiguration.resolvedArtifacts.forEach {
+//        if (it.moduleVersion.id.group == "org.lwjgl") {
+//            "runtime"("org.lwjgl:${it.moduleVersion.id.name}:${it.moduleVersion.id.version}:$lwjglNatives")
+//        }
+//    }
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+//tasks.withType<KotlinCompile> {
+//    kotlinOptions.jvmTarget = "1.8"
+//}
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,14 +1,118 @@
-import tornadofx.*
-import tornadofx.Stylesheet.Companion.button
-import tornadofx.Stylesheet.Companion.label
+import glm_.vec4.Vec4
+import gln.checkError
+import gln.glClearColor
+import gln.glViewport
+import imgui.Context
+import imgui.DEBUG
+import imgui.ImGui
+import imgui.impl.LwjglGlfw
+import org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT
+import org.lwjgl.opengl.GL11.glClear
+import org.lwjgl.system.MemoryStack
+import org.lwjgl.system.Platform
+import uno.glfw.GlfwWindow
+import uno.glfw.VSync
+import uno.glfw.glfw
 
-class Application: App(MainViewport::class)
+fun main(args: Array<String>) {
+    HelloWorld_lwjgl()
+}
 
-fun main(args: Array<String>) = launch<Application>(args)
+private class HelloWorld_lwjgl {
 
-class MainViewport: View() {
-    override val root = vbox {
-        button("Press me") { spacing = 8.0 }
-        label("Waiting") { spacing = 8.0 }
+    val window: GlfwWindow
+    val ctx: Context
+
+
+    var f = 0f
+    val clearColor = Vec4(0.45f, 0.55f, 0.6f, 1f)
+    var showAnotherWindow = false
+    var showDemo = true
+    var counter = 0
+
+    val lwjglGlfw: LwjglGlfw
+
+    init {
+
+        glfw.init(if (Platform.get() == Platform.MACOSX) "3.2" else "3.0")
+
+        window = GlfwWindow(1280, 720, "arcNet").apply {
+            init()
+        }
+
+        glfw.swapInterval = VSync.ON   // Enable vsync
+
+        ctx = Context()
+
+        ImGui.styleColorsDark()
+
+
+        // Setup Platform/Renderer bindings
+        lwjglGlfw = LwjglGlfw(window, true, LwjglGlfw.GlfwClientApi.OpenGL)
+
+        window.loop(::mainLoop)
+        lwjglGlfw.shutdown()
+        ctx.destroy()
+
+        window.destroy()
+        glfw.terminate()
+    }
+
+    fun mainLoop(stack: MemoryStack) {
+        lwjglGlfw.newFrame()
+        ImGui.run {
+
+            newFrame()
+            if (showDemo)
+                showDemoWindow(::showDemo)
+
+            run {
+
+                begin("Hello, world!")                          // Create a window called "Hello, world!" and append into it.
+
+                text("This is some useful text.")                // Display some text (you can use a format strings too)
+                checkbox("Demo Window", ::showDemo)             // Edit bools storing our window open/close state
+                checkbox("Another Window", ::showAnotherWindow)
+
+                sliderFloat("float", ::f, 0f, 1f)   // Edit 1 float using a slider from 0.0f to 1.0f
+                colorEdit3("clear color", clearColor)           // Edit 3 floats representing a color
+
+                if (button("Button"))                           // Buttons return true when clicked (most widgets return true when edited/activated)
+                    counter++
+
+                /*  Or you can take advantage of functional programming and pass directly a lambda as last parameter:
+                    button("Button") { counter++ }                */
+
+                sameLine()
+                text("counter = $counter")
+
+                text("Application average %.3f ms/frame (%.1f FPS)", 1_000f / io.framerate, io.framerate)
+
+                end()
+
+                // 3. Show another simple window.
+                if (showAnotherWindow) {
+                    // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+                    begin_("Another Window", ::showAnotherWindow)
+                    text("Hello from another window!")
+                    if (button("Close Me"))
+                        showAnotherWindow = false
+                    end()
+                }
+            }
+        }
+
+        // Rendering
+        ImGui.render()
+        glViewport(window.framebufferSize)
+        glClearColor(clearColor)
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        lwjglGlfw.renderDrawData(ImGui.drawData!!)
+
+        if (DEBUG)
+            checkError("mainLoop")
+
+//        RemoteryGL.rmt_EndOpenGLSample()
     }
 }
