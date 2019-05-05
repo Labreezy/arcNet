c6cc383e692bd5f826e8823cd1c1ee86b40200bc
aze <ajbtee@gmail.com>
2019-05-05 17:19:42 -0400

    Adding TornadoFX

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,60 +1,107 @@
-import application.WINDOW_HORZ
-import application.WINDOW_TINT
-import application.WINDOW_VERT
-import application.runApplicationLoop
-import gln.checkError
-import gln.glClearColor
-import gln.glViewport
-import imgui.Context
-import imgui.DEBUG
-import imgui.impl.LwjglGlfw
-import org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT
-import org.lwjgl.opengl.GL11.glClear
-import org.lwjgl.system.MemoryStack
-import uno.glfw.GlfwWindow
-import uno.glfw.glfw
-import imgui.ImGui as Ui
+import azUtils.pathHome
+import javafx.beans.property.SimpleStringProperty
+import javafx.scene.image.Image
+import javafx.scene.layout.BorderStrokeStyle
+import javafx.scene.shape.StrokeLineCap
+import javafx.scene.shape.StrokeLineJoin
+import javafx.scene.shape.StrokeType
+import session.Session
+import tornadofx.*
+
+private val session: Session = Session()
+fun getSession() = session
 
 fun main(args: Array<String>) {
-    Start_lwjgl()
+    launch<MyApp>(args)
 }
 
-lateinit var window: GlfwWindow
+class MyApp: App(MyView::class, Styles::class) {
+    init {
+        reloadStylesheetsOnFocus()
+    }
+}
 
-private class Start_lwjgl {
+class MyView : View() {
+    val controller: MyController by inject()
+    val input = SimpleStringProperty()
 
-    val context: Context
-    val lwjglGlfw: LwjglGlfw
+    override val root = form {
+        fieldset {
+            field("Input") {
+                input.value = ""
+                textfield(input)
+            }
+
+            button("Commit") {
+                action {
+                    if (!input.value.isEmpty()) {
+                        controller.writeToDb(input.value)
+                        input.value = ""
+                    }
+                }
+            }
+            piechart("Desktop/Laptop OS Market Share") {
+                data("Windows", 77.62)
+                data("OS X", 9.52)
+                data("Other", 3.06)
+                data("Linux", 1.55)
+                data("Chrome OS", 0.55)
+            }
+        }
+        val testImage = "${pathHome.toUri().toURL()}src/main/resources/ino_test.png"
+        println(testImage)
+        run {
+            val start = System.currentTimeMillis()
+            imageview(testImage) {
+
+                setPrefSize(512.0, 512.0)
+                println("loaded for ${System.currentTimeMillis() - start} msecs")
+            }
+            println("finished after ${System.currentTimeMillis() - start} msecs")
+        }
+    }
+}
+
+
+class MyController: Controller() {
+    fun writeToDb(inputValue: String) {
+        println("Writing $inputValue to database!")
+    }
+}
+
+
+class Styles : Stylesheet() {
+    companion object {
+        // Define our styles
+        val wrapper by cssclass()
+        val bob by cssclass()
+        val alice by cssclass()
+
+        // Define our colors
+        val dangerColor = c("#a94442")
+        val hoverColor = c("#d49942")
+    }
 
     init {
-        glfw.init("3.0")
-        DEBUG = false
+        wrapper {
+            padding = box(10.px)
+            spacing = 10.px
+        }
 
-        window = GlfwWindow(WINDOW_HORZ, WINDOW_VERT, "").apply { init() }
-        window.resizable = false
-        window.decorated = true
-        context = Context()
-        Ui.styleColorsDark()
-        lwjglGlfw = LwjglGlfw(window, true, LwjglGlfw.GlfwClientApi.OpenGL)
+        label {
+            fontSize = 16.px
+            padding = box(5.px, 10.px)
+            maxWidth = infinity
 
-        window.loop { mainLoop(it) }
+            and(bob, alice) {
+                borderColor += box(dangerColor)
+                borderStyle += BorderStrokeStyle(StrokeType.INSIDE, StrokeLineJoin.MITER, StrokeLineCap.BUTT, 10.0, 0.0, listOf(25.0, 5.0))
+                borderWidth += box(5.px)
 
-        lwjglGlfw.shutdown()
-        context.destroy()
-        window.destroy()
-        glfw.terminate()
-
+                and(hover) {
+                    backgroundColor += hoverColor
+                }
+            }
+        }
     }
-
-    fun mainLoop(stack: MemoryStack) {
-        lwjglGlfw.newFrame()
-        Ui.run { newFrame(); runApplicationLoop(stack) }
-        Ui.render()
-        glViewport(window.framebufferSize)
-        glClearColor(WINDOW_TINT)
-        glClear(GL_COLOR_BUFFER_BIT)
-        lwjglGlfw.renderDrawData(Ui.drawData!!)
-        if (DEBUG) checkError("mainLoop")
-    }
-
 }
